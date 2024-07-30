e151bb16148c4eeee23f4d7b69e2200faea81817
David Fradinho <davidfradinho@outlook.com>
2024-07-30 21:54:26 +0100

    Header mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { ref } from "vue";
+import router from "./../router";
 import json from "../json/navigation.json";
 
 const data = ref({
   json,
 });
+
+const refreshPage = () => {
+  router.go();
+};
 </script>
 
 <template>
@@ -16,12 +21,19 @@ const data = ref({
             data.json.logo.title
           }}</RouterLink>
         </p>
-        <nav>
-          <ul>
-            <li v-for="item in data.json.navigation">
-              <RouterLink :to="item.link">{{ item.title }}</RouterLink>
-            </li>
-          </ul>
+        <nav role="navigation">
+          <div class="header-menu">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul class="menu">
+              <li v-for="item in data.json.navigation" @click="refreshPage">
+                <RouterLink :to="item.link">{{ item.title }}</RouterLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </div>
@@ -29,6 +41,8 @@ const data = ref({
 </template>
 
 <style lang="scss" scoped>
+@import "../styles/variables.scss";
+
 header {
   padding: 20px 0;
   border-radius: 10px;
@@ -38,24 +52,115 @@ header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-  }
 
-  nav {
-    ul {
-      display: flex;
+    nav {
+      .header-menu {
+        input {
+          display: none;
 
-      li {
-        margin-right: 20px;
+          @media only screen and (max-width: 1024px) {
+            display: block;
+            width: 36px;
+            height: 30px;
+            position: absolute;
+            top: 24px;
+            right: 6%;
+            cursor: pointer;
+            opacity: 0;
+            z-index: 2;
 
-        &:last-of-type {
-          margin-right: 0;
+            cursor: pointer;
+
+            opacity: 0;
+            z-index: 2;
+
+            -webkit-touch-callout: none;
+          }
         }
 
-        a {
-          padding-bottom: 2px;
-          border-bottom: 2px solid transparent;
-          font-weight: 500;
-          font-size: 18px;
+        span {
+          @media only screen and (max-width: 1024px) {
+            display: block;
+            width: 34px;
+            height: 4px;
+            margin-bottom: 6px;
+            position: relative;
+
+            background: #cdcdcd;
+            border-radius: 3px;
+
+            z-index: 1;
+
+            @include animation;
+          }
+
+          &:last-of-type {
+            @media only screen and (max-width: 1024px) {
+              margin-bottom: 0;
+            }
+          }
+        }
+
+        input:checked ~ span {
+          @media only screen and (max-width: 1024px) {
+            width: 24px;
+          }
+        }
+        input:checked ~ span:nth-last-child(3) {
+          @media only screen and (max-width: 1024px) {
+            width: 34px;
+          }
+        }
+        input:checked ~ .menu {
+          @media only screen and (max-width: 1024px) {
+            display: flex;
+            transform: none;
+          }
+        }
+
+        .menu {
+          display: flex;
+
+          @media only screen and (max-width: 1024px) {
+            display: none;
+            position: absolute;
+            flex-direction: column;
+            align-items: flex-end;
+            top: 84px;
+            right: 10px;
+            width: 160px;
+            padding: 20px;
+            border-radius: 10px;
+            margin: auto;
+
+            background: $grey-dark;
+            list-style-type: none;
+            -webkit-font-smoothing: antialiased;
+          }
+
+          li {
+            margin-right: 20px;
+
+            @media only screen and (max-width: 1024px) {
+              margin-right: 0;
+              margin-bottom: 20px;
+            }
+
+            &:last-of-type {
+              margin-right: 0;
+
+              @media only screen and (max-width: 1024px) {
+                margin-bottom: 0;
+              }
+            }
+
+            a {
+              padding-bottom: 2px;
+              border-bottom: 2px solid transparent;
+              font-weight: 500;
+              font-size: 18px;
+            }
+          }
         }
       }
     }
